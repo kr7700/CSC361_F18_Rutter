a19d50818a9960a97e2c0dee47082bf1c982b5fe
Kevin Rutter <kr7700@cs.ship.edu>
2018-10-30 14:29:53 -0400

    Added and modified basic files from canyonbunny
    WorldController, WorldRenderer, Gamescreen etc.

--- core/src/com/packetpub/libgdx/rutter/CSC361_F18_Rutter.java
+++ core/src/com/packetpub/libgdx/rutter/CSC361_F18_Rutter.java
@@ -1,33 +1,34 @@
 package com.packetpub.libgdx.rutter;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.packetpub.libgdx.rutter.game.Assets;
+import com.packetpub.libgdx.rutter.screens.GameScreen;
+//import com.packetpub.libgdx.rutter.screens.MenuScreen;
+//import com.packetpub.libgdx.rutter.util.AudioManager;
+//import com.packetpub.libgdx.rutter.util.GamePreferences;
 
-public class CSC361_F18_Rutter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+/**
+ * The new Main class that works as an application listener
+ * 
+ * @author Tyler Forrester
+ */
 
+public class CSC361_F18_Rutter extends Game
+{
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create()
+	{
+		// Set Libgdx log level
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Load assets
+		Assets.instance.init(new AssetManager());
+		// Load preferences for audio settings and start playing music
+		//GamePreferences.instance.load();
+		//AudioManager.instance.play(Assets.instance.music.song01);
+		// Start game at game screen
+		setScreen(new GameScreen(this));
 	}
 }
